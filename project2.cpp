ef07252150e41cf8de710e562d29e1fe224689fb
cxl109120 <l_changsong@hotmail.com>
2018-03-12 22:12:23 -0500

    111

---
+++ project2.cpp
@@ -0,0 +1,28 @@
+//
+//  project2.cpp
+//  
+//
+//  Created by CHANGSONG LI on 3/12/18.
+//
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define num_receptionist = 1;
+#define num_patient = 3;
+
+int count = 0;
+
+
+
+
+
+
+
+
+
+
