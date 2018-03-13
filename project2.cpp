75d8f013658b7b52d9fbe4b58ac5d6b2ed6d8317
cxl109120 <l_changsong@hotmail.com>
2018-03-12 22:33:33 -0500

    11

--- project2.cpp
+++ project2.cpp
@@ -32,7 +32,7 @@ void* thread(void* arg)
     printf("\nEntered..\n");
     
     //critical section
-    sleep(4);
+    sleep(2);
     
     //signal
     printf("\nJust Exiting...\n");
@@ -45,8 +45,10 @@ int main()
     sem_init(&mutex, 0, 1);
     pthread_t t1,t2;
     pthread_create(&t1,NULL,thread,NULL);
-    sleep(2);
+    sleep(1);
+    
     pthread_create(&t2,NULL,thread,NULL);
+    
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
     sem_destroy(&mutex);
