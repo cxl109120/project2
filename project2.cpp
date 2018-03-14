d71db5398190717f2e4d102594bb9f6286eea1f0
cxl109120 <l_changsong@hotmail.com>
2018-03-14 10:51:20 -0500

    1

--- project2.cpp
+++ project2.cpp
@@ -183,11 +183,11 @@ int main(int argc, char* argv[])
     int doctor_num;
     for (int i = 0; i < num_doctor; i++)
     {
-        sem_wait(mutex1);
+        sem_wait(&mutex1);
         doctor_num = i;
         pthread_create(&doctor[i], NULL, doctor_thread, &doctor_num);
         pthread_create(&nurse[i], NULL, nurse_thread, &doctor_num);
-        sem_post(mutex1);
+        sem_post(&mutex1);
     }
     
     
